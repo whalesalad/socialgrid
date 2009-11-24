39865f4aafd30b0d92608cab520ad06373e23ab7
Michael Whalen <michael@whalesalad.com>
2009-11-23 20:16:57 -1000

    Added initial graphics and basic dir structure

--- socialgrid.php
+++ socialgrid.php
@@ -13,3 +13,23 @@ define('WP_DEBUG', true);
 define('SG_VERSION', 0.1);
 define('SG_NAME', 'SocialGrid');
 define('SG_SLUG', 'socialgrid');
+
+add_action('admin_menu', SG_SLUG.'_add_options_page');
+add_action('admin_post_'.SG_SLUG.'_save', SG_SLUG.'_save_options');
+add_action('init', SG_SLUG.'_settings_head');
+
+// if ($_GET['activated'])
+//     tasty_activate_theme();
+
+function socialgrid_add_options_page(){
+    add_theme_page(__(SG_NAME.' Options', SG_SLUG), __(SG_NAME.' Options', SG_SLUG), 'edit_themes', SG_SLUG.'-options', SG_SLUG.'_options_admin');
+}
+
+function socialgrid_settings_head() {
+    // wp_enqueue_style('tasty-settings-stylesheet', TASTY_STATIC . '/admin.css');
+    // wp_enqueue_script('tasty-admin-js', TASTY_STATIC . '/admin.js');
+}
+
+function socialgrid_options_admin() {
+    echo 'Meow';
+}
