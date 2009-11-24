56058ef5dd871b4d897f83966a2c108d100ea089
Michael Whalen <michael@whalesalad.com>
2009-11-23 18:44:42 -1000

    Initial commit of socialgrid

---
+++ socialgrid.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: SocialGrid
+Plugin URI: http://whalesalad.com/socialgrid
+Description: SocialGrid makes it easy to include attractive links to your various social media profiles on the web.
+Version: 0.1
+Author: Michael Whalen
+Author URI: http://whalesalad.com
+*/
+
+define('WP_DEBUG', true);
+
+define('SG_VERSION', 0.1);
+define('SG_NAME', 'SocialGrid');
+define('SG_SLUG', 'socialgrid');
